f8041af19b22132cd8501730b22a6996a57f77ae
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-19 15:32:57 -0400

    added new_contact

--- crm.rb
+++ crm.rb
@@ -12,6 +12,7 @@ require 'sinatra'
 # get "/" do
 #   "Hello World"
 # end
+@@rolodex = Rolodex.new
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -21,10 +22,10 @@ get "/contacts/new" do
   erb :new
 end
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+  # @contacts = []
+  # @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  # @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  # @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 
   erb :contacts
 end
