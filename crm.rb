3531794919fc68e2d881e3598feabf1d18d09267
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-19 15:55:48 -0400

    exercise completed

--- crm.rb
+++ crm.rb
@@ -18,7 +18,7 @@ get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
-get "/contacts/new" do
+get "/contacts/new_contact" do
   erb :new
 end
 get "/contacts" do
@@ -28,6 +28,15 @@ get "/contacts" do
   # @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 
   erb :contacts
+end
+get '/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  @@rolodex.add_contact(new_contact)
+  redirect to('/contacts')
 end
 
 
