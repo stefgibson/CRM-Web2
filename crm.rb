b9b66b4706e7148fdbb5ea4ee2d9497f283b50bb
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-19 15:14:13 -0400

    Added the contacts section

--- crm.rb
+++ crm.rb
@@ -1,12 +1,12 @@
+require_relative 'contact'
 require 'sinatra'
+# get '/' do
+#   "Main Menu"
+# end
 
-get '/' do
-  "Main Menu"
-end
-
-get '/' do
-  erb :index
-end
+# get '/' do
+#   erb :index
+# end
 
 # get "/" do
 #   "Hello World"
@@ -16,3 +16,16 @@ get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
+get "/contacts/new" do
+  erb :new
+end
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+  erb :contacts
+end
+
+
