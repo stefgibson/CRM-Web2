8f2447bd51320059cca66e243b6977e1e03cbb6b
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-21 22:35:41 -0400

    css styling done to homepage and nav

--- crm.rb
+++ crm.rb
@@ -15,7 +15,7 @@ require 'sinatra'
 @@rolodex = Rolodex.new
 
 get '/' do
-  @crm_app_name = "Stef's (Magical) CRM"
+  @crm_app_name = "Stef's CRM"
   erb :index
 end
 get "/contacts/new_contact" do
